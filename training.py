49da0d2c3cd22851388d479b67765f9a4104b349
Rajvir Bhatti <rajvirz2004@gmail.com>
2023-10-25 18:00:24 -0600

    new stuff

--- training.py
+++ training.py
@@ -15,7 +15,7 @@ from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
 from keras.optimizers import SGD
 
-lemmatizer = WordNetLemmatizer
+lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open("intents.json").read())
 
@@ -36,13 +36,43 @@ for word in words:
     if word in ignore_letters:
         words.remove(word)
 
-print(words)
+words = sorted(set(words))
 
+classes = sorted(set(classes))
 
-# lemmatized_words = [
-#     lemmatizer.lemmatize(word) for word in words if word not in ignore_letters
-# ]
+pickle.dump(words, open("words.pkl", "wb"))
+pickle.dump(words, open("classes.pkl", "wb"))
 
-# lemmatized_words = sorted(set(words))
+training = []
+output_empty = [0] * len(classes)
 
-# print(lemmatized_words)
+for document in documents:
+    bag = []
+    word_patterns = document[0]
+    word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    for word in words:
+        bag.append(1) if word in word_patterns else bag.append(0)
+
+    output_row = list(output_empty)
+    output_row[classes.index(document[1])] = 1
+    training.append([bag, output_row])
+
+random.shuffle(training)
+training = np.array(training)
+
+train_x = list(training[:, 0])
+train_y = list(training[:, 1])
+
+model = Sequential()
+model.add(Dense(128, input_shape=(len(train_x[0]),), activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(64, activation="relu"))
+model.add(Dropout(0.5))
+model.add(Dense(len(train_y[0]), activation="softmax"))
+
+sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accurate"])
+
+model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
+model.save("chatbot_model.model")
+print("done")
