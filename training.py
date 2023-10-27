e13e93745ba07447f5fbc24ab5df5151fe13bfa9
Rajvir Bhatti <rajvirz2004@gmail.com>
2023-10-27 01:06:06 -0600

    neural network done and trained

--- training.py
+++ training.py
@@ -10,6 +10,7 @@ nltk.download("wordnet")
 nltk.download("omw-1.4")
 from nltk.stem import WordNetLemmatizer
 
+import tensorflow as tf
 from tensorflow import keras
 from keras.models import Sequential
 from keras.layers import Dense, Activation, Dropout
@@ -58,7 +59,10 @@ for document in documents:
     training.append([bag, output_row])
 
 random.shuffle(training)
-training = np.array(training)
+
+data_type = object
+
+training = np.array(training, dtype=data_type)
 
 train_x = list(training[:, 0])
 train_y = list(training[:, 1])
@@ -70,8 +74,10 @@ model.add(Dense(64, activation="relu"))
 model.add(Dropout(0.5))
 model.add(Dense(len(train_y[0]), activation="softmax"))
 
-sgd = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
-model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accurate"])
+
+
+sgd = tf.keras.optimizers.legacy.SGD(learning_rate=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+model.compile(loss="categorical_crossentropy", optimizer=sgd, metrics=["accuracy"])
 
 model.fit(np.array(train_x), np.array(train_y), epochs=200, batch_size=5, verbose=1)
 model.save("chatbot_model.model")
